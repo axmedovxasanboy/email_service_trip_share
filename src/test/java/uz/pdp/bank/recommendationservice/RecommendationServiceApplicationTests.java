da4dbf18440d94f70210dd53474040208d2ff7e5
jack-sparrow-18-04 <akhmedovkhasanboy28@gmail.com>
2024-09-27 11:51:59 +0500

    first commit

---
+++ src/test/java/uz/pdp/bank/recommendationservice/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.bank.recommendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
